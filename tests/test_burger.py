f2703232ffb630d462c2433db743e58ded280dc8
Evgeny Krysenkov <eugene.krysenkov@yandex.com>
2024-06-02 13:24:06 +0300

    Коммит №3 Создание объектов перенесены в тесты, все тесты успешно проходят.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,18 +1,21 @@
 import data.name_variables
 import pytest
 from unittest.mock import Mock
+from praktikum.burger import Burger
 
 
 class TestBurger:
     @pytest.mark.parametrize('name_of_bun', ["чизбургер", "гамбургер"])
-    def test_set_buns(self, name_of_bun, some_burger):
+    def test_set_buns(self, name_of_bun):
+        some_burger = Burger()
         some_burger.set_buns(name_of_bun)
         expected = some_burger.bun
 
         assert expected == name_of_bun
 
     @pytest.mark.parametrize('ingredient', ["лунный огурец", "адаманитовый помидор"])
-    def test_add_ingredient(self, ingredient, some_burger):
+    def test_add_ingredient(self, ingredient):
+        some_burger = Burger()
         some_burger.add_ingredient(ingredient)
         expected = some_burger.ingredients
 
@@ -20,7 +23,8 @@ class TestBurger:
 
     @pytest.mark.parametrize('ingredient_one, ingredient_two ',
                              [["лунный огурец", "адаманитовый помидор"], ["марсовый перец", "юпитерский сыр"]])
-    def test_remove_ingredient(self, ingredient_one, ingredient_two, some_burger):
+    def test_remove_ingredient(self, ingredient_one, ingredient_two):
+        some_burger = Burger()
         some_burger.add_ingredient(ingredient_one)
         some_burger.add_ingredient(ingredient_two)
         some_burger.remove_ingredient(0)
@@ -30,7 +34,8 @@ class TestBurger:
 
     @pytest.mark.parametrize('ingredient_one, ingredient_two ',
                              [["лунный огурец", "адаманитовый помидор"], ["марсовый перец", "юпитерский сыр"]])
-    def test_move_ingredient(self, ingredient_one, ingredient_two, some_burger):
+    def test_move_ingredient(self, ingredient_one, ingredient_two):
+        some_burger = Burger()
         some_burger.add_ingredient(ingredient_one)
         some_burger.add_ingredient(ingredient_two)
         some_burger.move_ingredient(1, 0)
@@ -39,7 +44,10 @@ class TestBurger:
 
         assert expected == result
 
-    def test_get_price(self, some_burger, mock_bun, mock_ingredient):
+    def test_get_price(self):
+        some_burger = Burger()
+        mock_bun = Mock()
+        mock_ingredient = Mock()
         mock_bun.get_price.return_value = data.name_variables.mock_bun_price
         mock_ingredient.get_price.return_value = data.name_variables.mock_ingredient_price
         some_burger.bun = mock_bun
@@ -48,7 +56,10 @@ class TestBurger:
 
         assert expected == 400
 
-    def test_get_receipt(self, some_burger, mock_bun, mock_ingredient):
+    def test_get_receipt(self):
+        some_burger = Burger()
+        mock_bun = Mock()
+        mock_ingredient = Mock()
         mock_bun.get_name.return_value = data.name_variables.mock_bun_name
         mock_ingredient.get_name.return_value = data.name_variables.mock_ingredient_name
         mock_ingredient.get_type.return_value = data.name_variables.mock_ingredient_type
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,16 +1,22 @@
 import data.name_variables
+from praktikum.database import Database
+from unittest.mock import Mock
 
 
 class TestDatabase:
 
-    def test_available_buns(self, mock_database, some_database):
+    def test_available_buns(self):
+        some_database = Database()
+        mock_database = Mock()
         mock_database.available_buns.return_value = data.name_variables.mock_db_bun
         some_database.available_buns = mock_database.available_buns
         expected = some_database.available_buns()
 
         assert expected == ["Булочка с начинкой", "Булочка с грибами"]
 
-    def test_available_ingredients(self, mock_database, some_database):
+    def test_available_ingredients(self):
+        some_database = Database()
+        mock_database = Mock()
         mock_database.available_ingredients.return_value = data.name_variables.mock_db_ingredient
         some_database.available_ingredients = mock_database.available_ingredients
         expected = some_database.available_ingredients()
